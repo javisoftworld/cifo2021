c24de97915c13502f21ff1bac2f5689ce79c7c2e
javisoftworld <94255977+javisoftworld@users.noreply.github.com>
2021-11-16 19:22:39 +0100

    Subir al servidor el cambio en PlayerController

--- PlayerController.cs
+++ PlayerController.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     
+	public bool estaVivo;
+	
     // Start is called before the first frame update
     void Start()
     {
-        
+        estaVivo=true;
     }
 
     // Update is called once per frame

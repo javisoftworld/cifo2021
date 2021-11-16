20447ce9729279e9b395c11e87e238de86462d68
aprendeaprogramarya <94445307+aprendeaprogramarya@users.noreply.github.com>
2021-11-16 19:20:29 +0100

    Modifico el fichero Player
    he creado una variable llamada velocidad

--- PlayerController.cs
+++ PlayerController.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
+    public float velocidad;
     // Start is called before the first frame update
     void Start()
     {
-        
+        velocidad=5.0f;
     }
 
     // Update is called once per frame
